46884eb33fd326ae495fcb2a7c2b8ce45100af0d
Leowenex <antoinecraipeau@outlook.fr>
2022-04-20 09:52:01 +0200

    preset Manager Developpement

--- tfm-frontend/src/postauth/library.js
+++ tfm-frontend/src/postauth/library.js
@@ -12,6 +12,7 @@ align:left;
 const LeLibraryListed = styled.a`
 margin: 5px 5px;
 border: solid black;
+border-radius: 3px;
 padding: 2px 2px 2px 2px;
 width: 85vw;
 height: 15vh;
@@ -19,6 +20,7 @@ color: grey;
 font-size: 30px;
 text-align: left;
 background-color: white;
+display: flex;
 `
 const LibraryButton = styled.button`
 border: 2px solid red;
@@ -28,13 +30,37 @@ margin-right: 10px;
 font-size: 20px;
 color: red;
 `
-
 const PHeader = styled.div`
 background-color: grey;
 height: 4vh;
 font-size: 3vh;
 margin-bottom: 15px;
 `
+const TagList = styled.div`
+border: 1px solid black;
+border-radius: 3px;
+display: flex;
+flex-wrap: wrap;
+width: 40vw;
+margin-left: auto;
+`
+const Tagged = styled.div`
+height: 3vh;
+font-size: 2vh;
+border: 1px solid black;
+border-radius: 3px;
+padding: 2px 2px 2px 2px;
+margin: 1px 1px 1px 1px;
+`
+const GameResearch = styled.input`
+height: 3vh;
+width: 9vw;
+border: 1px solid black;
+border-radius: 3px;
+padding: 2px 2px 2px 2px;
+margin: 1px 1px 1px 1px;
+`
+
 
 function LibraryList(){
     return(
@@ -42,8 +68,12 @@ function LibraryList(){
             <LeLibraryList>
                {userdata[0].games.map((game)=>(
                <LeLibraryListed key={game.id} onClick={() => {}} >
-                    {game.title}
-                    <div><LibraryButton>Specify Time</LibraryButton><LibraryButton>Remove Game</LibraryButton></div>
+                    <div>
+                        {game.title}
+                        <div><LibraryButton>Specify Time</LibraryButton><LibraryButton>Remove Game</LibraryButton></div>
+                    </div>
+                    <div>
+                    </div>
                </LeLibraryListed>))} 
             </LeLibraryList>
         </div>
@@ -63,11 +93,20 @@ function PresetList(){
     return(
         <div>
             <LeLibraryList>
-               {userdata[0].games.map((game)=>(
-               <LeLibraryListed key={game.id} onClick={() => {}} >
-                    {game.title}
-                    <div><LibraryButton>Edit</LibraryButton><LibraryButton>Delete</LibraryButton></div>
-               </LeLibraryListed>))} 
+                {userdata[0].games.map((game)=>(
+                <LeLibraryListed key={game.id} onClick={() => {}} >
+                    <div>
+                        {game.title}
+                        <div><LibraryButton>Edit</LibraryButton><LibraryButton>Delete</LibraryButton></div>
+                    </div>
+                    <TagList>
+                        {userdata[0].games.map((game)=>(
+                            <Tagged key={game.id}>{game.title}</Tagged>
+                        ))}
+                        <GameResearch label="Search">
+                        </GameResearch>
+                    </TagList> 
+                </LeLibraryListed>))} 
             </LeLibraryList>
         </div>
         
