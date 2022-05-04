9e05d372a1e1d14eee3525cd23840ec78f5e0054
Elliot Maisl <elliot.maisl@gmail.com>
2022-05-04 14:14:25 +0200

    Add image URLs to games

---
+++ backend/migrations/20220502163406_game_image.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.alterTable('game', t => {
+    t.string('image_url').defaultTo(null);
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.alterTable('game', t => {
+    t.dropColumn('image_url');
+  })
+};
